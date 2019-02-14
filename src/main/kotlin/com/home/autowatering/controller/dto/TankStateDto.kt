54c1c31f30a3ce177df5375ae3b0137d5a1d51f7
Ekaterina <ekaterina.gerasimova@inbox.ru>
2019-02-14 21:01:45 +0700

    fixed tank dto deserialization

--- src/main/kotlin/com/home/autowatering/controller/dto/TankStateDto.kt
+++ src/main/kotlin/com/home/autowatering/controller/dto/TankStateDto.kt
@@ -5,14 +5,24 @@ import com.fasterxml.jackson.annotation.JsonInclude
 import java.util.*
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-data class TankStateDto(
-    val id: Long? = null,
-    val name: String,
+class TankStateDto {
+    var id: Long? = null
+    lateinit var name: String
     @JsonFormat(
         shape = JsonFormat.Shape.STRING,
         pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"
     )
-    val date: Date?,
-    val volume: Double,
-    val filled: Double
-)
+    lateinit var date: Date
+    var volume: Double = 0.0
+    var filled: Double = 0.0
+
+    constructor()
+
+    constructor(id: Long? = null, name: String, date: Date, volume: Double, filled: Double) {
+        this.id = id
+        this.name = name
+        this.date = date
+        this.volume = volume
+        this.filled = filled
+    }
+}
